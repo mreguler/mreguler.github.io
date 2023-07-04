e0824b1cce918417f178ce6fca27f1434f726ab3
emreguler <eguler@sas.upenn.edu>
2023-07-03 17:40:52 -0700

    Added dropdown menus

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
-window.onload = function() {
-    const years = Array.from(document.querySelectorAll('.toolbar span[data-year]'));
-    const locations = Array.from(document.getElementsByClassName('location'));
-    const images = Array.from(document.getElementsByClassName('content-item'));
-    
-    years.forEach((year) => {
-        year.addEventListener('click', function() {
-            // Add strikethrough to selected year and remove from others
-            years.forEach((yr) => {
-                if (yr === this) {
-                    yr.classList.add('active');
-                } else {
-                    yr.classList.remove('active');
-                }
-            });
-        });
+function filterLocation(location) {
+    let items = document.querySelectorAll('.content-item');
+    items.forEach((item) => {
+        if (location === "all") {
+            item.style.display = "";
+        } else if (item.getAttribute('data-location') !== location) {
+            item.style.display = "none";
+        } else {
+            item.style.display = "";
+        }
     });
+}
 
-    locations.forEach((location) => {
-        location.addEventListener('click', function() {
-            let selectedLocation = this.getAttribute('data-location');
-
-            // Add strikethrough to selected location and remove from others
-            locations.forEach((loc) => {
-                if (loc === this) {
-                    loc.classList.add('active');
-                } else {
-                    loc.classList.remove('active');
-                }
-            });
-
-            // Show only images that match the selected location
-            images.forEach((img) => {
-                if (selectedLocation === 'all' || img.getAttribute('data-location') === selectedLocation) {
-                    img.style.display = 'block';
-                } else {
-                    img.style.display = 'none';
-                }
-            });
-        });
-    });
+function filterYear(year) {
+    if(year === "all") {
+        window.location.href = "index.html";
+    } else {
+        window.location.href = year + ".html";
+    }
 }
